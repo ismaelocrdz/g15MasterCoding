c9db09384d6d944da6f103cbdd43432532a48c78
Ismael Ochoa <71686085+ismaelocrdz@users.noreply.github.com>
2022-03-08 20:30:04 -0600

    Add files via upload

---
+++ 02_KataJS/02/02.js
@@ -0,0 +1,42 @@
+//Declrar variable
+var edad=30
+/*Se asigna un variable y se hace una operación con la asignación de esa varible*/
+var total=edad*2
+//Mostrando información de salida
+console.log(total)
+
+
+var dato=prompt("Ingresa tu edad: ")
+var opera=dato+2
+console.log(operacion)
+
+var dato=prompt("Ingresa tu edad: ")
+typeof dato
+
+/*PAra evitar la concatenación hay que convertir una variable string en numérica*/
+var dato=Number(prompt("Ingresa tu edad: "))
+var opera=dato+2
+console.log(opera)
+
+
+//Tarea
+var dato=Number(prompt("Ingresa un número:"))
+var opera=dato*2
+console.log(opera)
+
+var num01=Number(prompt("Dame un número:"))
+var num02=Number(prompt("Dame otro número:"))
+var opera=num01-num02
+console.log(opera)
+
+Math.PI*5.3**2
+
+var D=8
+var d=6
+var opera=(8*6)/2
+console.log(opera)
+
+var D=8
+var d=6
+var opera=(30*16)/2
+console.log(opera)
